f60ad7d10cf074e43075399da559084ba3152e9a
Sue Sujeong Cha <39662008+sujeongcha@users.noreply.github.com>
2019-11-26 09:08:02 -0500

    Create Q4.py

---
+++ String Exercise/Q4.py
@@ -0,0 +1,30 @@
+#arrange String characters such that lowercase letters should come first
+
+#My Solution
+def lowerFirst(s1):
+  lower = ""
+  upper = ""
+  for a in s1:
+    if a.islower():
+      lower += a
+    else:
+      upper += a
+  print("input_String =", s1)
+  print("arranging characters giving precedence to lower case letters:")
+  print(lower+upper)
+  
+lowerFirst("PyNaTive")
+
+#Given Solution
+inputStr = "PyNaTive"
+words = inputStr.split()
+lower = []
+upper = []
+for char in inputStr:
+    if char.islower():
+        lower.append(char)
+    else:
+        upper.append(char)
+sortedString = ''.join(lower + upper)
+print("\n arranging characters giving precedence to lowercase letters:")
+print(sortedString)
