3d2a238fd6cdcd5233c50eea99185e91373585c4
Sue Sujeong Cha <39662008+sujeongcha@users.noreply.github.com>
2019-12-24 10:49:17 -0500

    secrets.SystemRandom()

---
+++ Random Data Generation/Q3.py
@@ -0,0 +1,12 @@
+#Generate 6 digit random secure OTP
+
+#Given Solution
+import secrets
+
+#Getting systemRandom class instance out of secrets module
+secretsGenerator = secrets.SystemRandom()
+
+print("Generating 6 digit random OTP")
+otp = secretsGenerator.randrange(100000, 999999)
+
+print("Secure random OTP is ", otp)
